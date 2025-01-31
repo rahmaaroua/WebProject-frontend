7fc4512f2692f43672d0b8cb071e897fef90dbd1
Narmine <narminehaddad1234@gmail.com>
2025-01-31 23:29:57 +0100

    Merge resolved conflicts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
